45491e4913ec5761a76dcaf27f4e9fa416d50463
Roberto Samia <bojosteph@gmail.com>
2018-09-14 23:48:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def is_prime?(number)
-  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+   (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
   return true
 end
 
